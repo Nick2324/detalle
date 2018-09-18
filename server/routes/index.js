b36067fbb9507781c71ecdff7aff99632940b1df
ngarcia <ngarcia@linix.com.co>
2018-09-18 18:00:25 -0500

    Se agrega menu lateral y prototipo de datos del menu principal

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+                        tituloEntrada: "Para mi persona especial",
+                        descripcionEntrada: "Elije el tema que quieras revivir de nuestros ultimos momentos",
+                        menu:[{nombre:"Nuestras rutas"},
+                              {nombre:"Nuestros momentos"}, 
+                              {nombre:"Nuestras canciones"},
+                              {nombre:"Mis dedicatorias"},
+                              {nombre:"Me inspiras..."}]
+                      });
 });
 
 router.get('/mapa', function (req, res, next) {
