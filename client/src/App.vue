c1c742819d42ae55fc1da2bf8f2f5c75807bf009
ngarcia <ngarcia@linix.com.co>
2018-10-05 07:16:02 -0500

    Avance Menu.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!--router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link-->
+      <Menu />
     </div>
-    <router-view/>
+    <!--router-view/-->
   </div>
 </template>
 
@@ -29,3 +30,16 @@
   color: #42b983;
 }
 </style>
+
+<script>
+  // @ is an alias to /src
+  import Menu from '@/components/Menu.vue'
+
+  export default {
+    name: 'root',
+    components: {
+      Menu
+    }
+  }
+</script>
+
---
+++ client/src/components/Menu.vue
@@ -0,0 +1,107 @@
+<template>
+<div class="menu">
+  <div id="mySidenav" v-bind:class="clases">
+    <a @click="toggle" href="#">&#9776;</a>
+    <ul>
+      <li v-for="menu in menus" v-bind:key="menu.id">
+        <a href="#">{{menu.nombre}}</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<style>
+.open-sidenav{
+  font-size:30px;cursor:pointer
+}
+
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+
+.sidenav-opened{
+  width:0px;
+}
+
+.sidenav-closed{
+  width:250px;
+}
+</style>
+
+<script>
+export default {
+  name: 'menu',
+  // props: ["menus"],
+  data () {
+    return {
+      closed: false,
+      menus: [
+        { nombre: 'Nuestras rutas', id: 1 },
+        { nombre: 'Nuestros momentos', id: 2 },
+        { nombre: 'Nuestras canciones', id: 3 },
+        { nombre: 'Mis dedicatorias', id: 4 },
+        { nombre: 'Me inspiras...', id: 5 }
+      ]
+    }
+  },
+  computed: {
+    classes () {
+      return {
+        'sidenav-opened': !this.closed,
+        'sidenav-closed': this.closed,
+        'sidenav': true
+      }
+    }
+  },
+  methods: {
+    toggle () {
+      this.closed = !this.closed
+    }
+  }
+}
+</script>
